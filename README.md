07b9a94dd0956bbbecb5ab95fe65ee9189c558f6
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-12 21:22:52 +0200

    catalog

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
-# React TypeScript Starter Pack
+# React Phone Catalog
+Welcome to React Phone Catalog! This is a single page application built with React.js, featuring routers, context, and more to showcase a catalog of phones.
 
-To use this template click `Use this template`
+# Features
+1. Dynamic Routing: Utilizes React Router for navigation, enabling seamless transitions between different views of the catalog.
+2. Context Management: Utilizes React Context API for state management, ensuring efficient data flow throughout the application.
+3. Responsive Design: Designed with responsiveness in mind, providing an optimal viewing experience across a wide range of devices.
+4. Filtering and Sorting: Implements functionality to filter and sort the phone catalog based on various attributes, enhancing user experience.
+5. Detailed Phone Information: Provides detailed information about each phone, including specifications, description, and images.
 
-### Available Scripts
+# Technologies Used
+1. React: JavaScript library for building user interfaces.
+2. React Router: Declarative routing for React applications.
+3. React Context API: For state management across the application.
+4. HTML & CSS (SCSS): For markup and styling.
+5. JavaScript (ES6+): The primary programming language used.
+6. Git: Version control system for tracking changes.
+7. GitHub Pages: Hosting platform for deploying the application.
 
-`Deploy` - available to deploy your application to gh-pages
+# Contributing
+Contributions are welcome! If you'd like to contribute to this project, please follow these steps:
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+1. Fork the repository.
+2. Create your feature branch (git checkout -b feature/YourFeature).
+3. Commit your changes (git commit -am 'Add some feature').
+4. Push to the branch (git push origin feature/YourFeature).
+5. Create a new Pull Request.
+
+# Project Link
+Check out the live demo [here](https://DenisGurskiy.github.io/Phone-catalog_React/).
+
+# Design Link
+- [Design Link](https://www.figma.com/file/7JTa0q8n3dTSAyMNaA0u8o/Phone-catalog-(V2)-Rounded-Style-3)
+
+# Author
+This project was developed by Denys Hurskiy.
+
+# License
+This project is licensed under the MIT License.
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { CatalogProvider } from './components/Contexts/CatalogContext';
+import { Header } from './components/Header/Header';
+import { HomePage } from './components/HomePage';
+import { Footer } from './components/Footer';
+import { NotFoundPage } from './components/NotFoundPage';
+import { ProductPage } from './components/ProductPage';
+import { DetailsPage } from './components/DetailsPage';
+import { CartPage } from './components/CartPage';
+import { FavouritesPage } from './components/FavouritesPage';
+import { ContactsPage } from './components/ContactsPage';
+import { RightsPage } from './components/RightsPage';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <CatalogProvider>
+      <div className="App page__app">
+        <Header />
+
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="/phones" element={<ProductPage category={'Phones'} />} />
+          <Route path="/phones/:productId" element={<DetailsPage />} />
+
+          <Route
+            path="tablets"
+            element={<ProductPage category={'Tablets'} />}
+          />
+          <Route path="tablets/:productId?" element={<DetailsPage />} />
+
+          <Route
+            path="accessories"
+            element={<ProductPage category={'Accessories'} />}
+          />
+          <Route path="accessories/:productId?" element={<DetailsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/rights" element={<RightsPage />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </CatalogProvider>
   );
 };
